16b4c18ff74f514eab7df85cf04e52865b110a31
Yashwanth <yashwanthsuresh8@gmail.com>
2026-04-02 13:00:32 +0530

    created route and install ejs

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+const app = express();
+const port = 3200;
+
+app.set('view engine','ejs')
+
+app.get('/',(req,res)=>{
+  res.render('list')
+})
+
+app.get('/add',(req,res)=>{
+  res.render('add')
+})
+
+app.get('/update',(req,res)=>{
+  res.render('update')
+})
+
+app.listen(port,()=>{
+  console.log("Server is running ",port)
+})
