e4056275ee6a705a6ededf020cf4525f6c42adde
Yashwanth <yashwanthsuresh8@gmail.com>
2026-04-02 22:05:30 +0530

    created database and connected with backend

--- backend/index.js
+++ backend/index.js
@@ -1,33 +1,53 @@
-import express from 'express';
-import path from 'path'
+import express from "express";
+import { MongoClient } from "mongodb";
+import path from "path";
 const app = express();
 const port = 3200;
 
-const publicPath  = path.resolve('public')
-app.use(express.static(publicPath))
+const publicPath = path.resolve("public");
+app.use(express.static(publicPath));
 
-app.set('view engine','ejs')
+app.set("view engine", "ejs");
 
-app.get('/',(req,res)=>{
-  res.render('list')
-})
+const dbName = "node-project";
+const collectionName = "todo";
+const url = "mongodb://localhost:27017";
+const client = new MongoClient(url);
 
-app.get('/add',(req,res)=>{
-  res.render('add')
-})
+const connection = async () => {
+  const connect = await client.connect();
+  return connect.db(dbName);
+};
 
-app.get('/update',(req,res)=>{
-  res.render('update')
-})
+app.use(express.urlencoded({ extended: false }));
 
-app.post('/add',(req,res)=>{
-  res.redirect('/');
-})
+app.get("/", (req, res) => {
+  res.render("list");
+});
 
-app.post('/update',(req,res)=>{
-  res.redirect('/')
-})
+app.get("/add", (req, res) => {
+  res.render("add");
+});
 
-app.listen(port,()=>{
-  console.log("Server is running ",port)
-})
+app.get("/update", (req, res) => {
+  res.render("update");
+});
+
+app.post("/add", async (req, res) => {
+  const db = await connection();
+  const collection = db.collection(collectionName);
+  const result = await collection.insertOne(req.body);
+  if (result) {
+    res.redirect("/");
+  } else {
+    res.redirect("/add");
+  }
+});
+
+app.post("/update", (req, res) => {
+  res.redirect("/");
+});
+
+app.listen(port, () => {
+  console.log("Server is running ", port);
+});
